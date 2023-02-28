45edf7eaaf86cb91288797a649c0afff6fbd7818
santhosh2808 <santhosh.devendran@fwsa.freshworks.com>
2023-02-28 14:14:54 +0530

    post CRU completed

---
+++ output.js
@@ -0,0 +1,44 @@
+// let a=[4,3,17,10,11,12];
+// let sandy=0;
+// let k=3;
+// let max=0
+
+//  3rd sum ////////////////
+
+
+// for (let i=0;i<a.length;i++){
+//      if(a[i]%2==0){
+//         sandy= sandy+a[i]
+//      }
+// }
+// console.log(sandy)
+
+//  4th sum /////////////////////
+
+// let b=[4,5,10,11,1]
+
+// for (let i=0;i<k;i++){
+//     for(let j=i;j<i+k;j++){
+//       sandy+=b[j];
+//       if(sandy > max){
+//         max = sandy
+//     }
+//     } 
+//     sandy=0;
+// }console.log(max)
+
+// 1st sum    ////////
+
+let n=5
+let str
+// for(let i=1;i<11;i++){
+//     str=i+"*"+n+"="+ i*n;
+//     console.log(str)
+// }
+
+for(let i=1;i<11;i++){
+  if(i!==n){
+    str=i
+  }
+}
+console.log(str)
